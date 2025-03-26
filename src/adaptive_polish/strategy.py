d88bd917a4470412804c578f6c62608c1a119078
Thomas M Fish <50103643+thomasmfish@users.noreply.github.com>
2025-03-26 18:42:11 +0000

    Add missing import

--- src/adaptive_polish/strategy.py
+++ src/adaptive_polish/strategy.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 import logging
 from dataclasses import dataclass
 from pathlib import Path
+from matplotlib.colors import ListedColormap
 import matplotlib.pyplot as plt
 import numpy as np
 import typing
