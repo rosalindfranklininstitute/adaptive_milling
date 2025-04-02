f663d9f4ded9d883df502cb6f798dc26ffe4bbd9
Thomas M Fish <50103643+thomasmfish@users.noreply.github.com>
2025-04-02 18:02:04 +0100

    Add test to check model segmentation handles both images and paths

--- tests/test_segmentation.py
+++ tests/test_segmentation.py
@@ -1,4 +1,7 @@
+import pytest
+
 from pathlib import Path
+import tifffile
 
 from adaptive_polish import gis_measurement as gm
 
@@ -7,13 +10,19 @@ def test_segmentation_model_loads(sem_segmentation_model: tuple[str, Path]):
     """Test that the segmentation model produces a prediction"""
     gm.load_sem_model(model_path, generation=generation)
 
-
+@pytest.mark.parametrize(["as_array"], [True, False], ids=["array", "path"])
 def test_segmentation_model_runs(
-    sem_segmentation_model: tuple[str, Path], sem_image_path: Path
+    as_array: bool, sem_segmentation_model: tuple[str, Path], sem_image_dir: Path
 ):
     """Test that the segmentation model produces a prediction"""
     generation, model_path = sem_segmentation_model
     model = gm.load_sem_model(model_path, generation=generation)
-    prediction = model.predict(sem_image_path)
+    image_path = next(sem_image_dir.glob("*.tif"))
+    if as_array:
+        image = tifffile.imread(image_path)
+    else:
+        image = image_path
+
+    prediction = model.predict(image)
     assert prediction.min() == 0
     assert prediction.max() == model.num_classes - 1
