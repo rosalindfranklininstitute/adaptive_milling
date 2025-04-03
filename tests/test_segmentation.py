9d6e04039e31cb58f08abd904c57e3b052424ee5
Thomas M Fish <50103643+thomasmfish@users.noreply.github.com>
2025-04-03 15:58:27 +0100

    Test segmentation model handles both arrays and paths

--- tests/test_segmentation.py
+++ tests/test_segmentation.py
@@ -5,12 +5,14 @@ import tifffile
 
 from adaptive_polish import gis_measurement as gm
 
+
 def test_segmentation_model_loads(sem_segmentation_model: tuple[str, Path]):
     generation, model_path = sem_segmentation_model
     """Test that the segmentation model produces a prediction"""
     gm.load_sem_model(model_path, generation=generation)
 
-@pytest.mark.parametrize(["as_array"], [True, False], ids=["array", "path"])
+
+@pytest.mark.parametrize(["as_array"], [[True], [False]], ids=["array", "path"])
 def test_segmentation_model_runs(
     as_array: bool, sem_segmentation_model: tuple[str, Path], sem_image_dir: Path
 ):
