e4b70f63dcac81f3a09bdd428703bae5ffedd121
Thomas M Fish <50103643+thomasmfish@users.noreply.github.com>
2025-04-04 16:49:56 +0100

    Simplify integration test (remove Experiment stuff to be be tested elsewhere)

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 import pytest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 
 import functools
 import typing
@@ -8,11 +8,11 @@ from pathlib import Path
 
 from fibsem import utils, acquire
 from fibsem.milling import get_milling_stages, mill_stages
+
 # TODO: look into why this is required
 # Necessary to ensure AP strategy is registered:
 from fibsem.milling.strategy import register_strategy
 from autolamella.protocol.validation import validate_protocol
-from autolamella.structures import Experiment, AutoLamellaProtocol
 from adaptive_polish.strategy import AdaptivePolishMillingConfig
 
 from . import setup
@@ -65,11 +65,6 @@ def protocol_path(
     )
 
 
-@pytest.fixture
-def experiment_config_path(experiment_template_path, tmp_path: Path):
-    return setup.setup_test_experiment(experiment_template_path, tmp_path)
-
-
 def raise_error_after_num_calls(
     fn: typing.Callable,
     calls_before_exception: int,
@@ -92,7 +87,6 @@ def raise_error_after_num_calls(
 
 def test_runs(
     microscope_config_path: Path,
-    experiment_config_path: Path,
     protocol_path: Path,
     tmp_path: Path,
 ) -> None:
@@ -102,73 +96,56 @@ def test_runs(
     # connect to microscope
     microscope, settings = utils.setup_session(config_path=microscope_config_path)
 
-    original_stop_milling = microscope.stop_milling
+    protocol = validate_protocol(utils.load_protocol(protocol_path=protocol_path))
 
-    with patch.object(microscope, "stop_milling") as mock_stop_milling:
-        mock_stop_milling.side_effect = raise_error_after_num_calls(
-            original_stop_milling, calls_before_exception=calls_before_exception
+    milling_stages = protocol.milling["mill_polishing"]
+    protocol_strategy_config: AdaptivePolishMillingConfig = milling_stages[
+        0
+    ].strategy.config
+    for k, v in _AP_MILLING_CONFIG_SETTINGS.items():
+        value = getattr(protocol_strategy_config, k)
+        assert getattr(protocol_strategy_config, k) == v, (
+            f"Strategy config does not match expected value: {k} is {value}, expected {v}"
         )
 
-        protocol = validate_protocol(utils.load_protocol(protocol_path=protocol_path))
+    lamella_directory = tmp_path / "lamella"
+    lamella_directory.mkdir()
+    adaptive_polish_dir = lamella_directory / "adaptive_polish"
 
-        lamella_directory = tmp_path / "lamella"
-        lamella_directory.mkdir()
-        adaptive_polish_dir = lamella_directory / "adaptive_polish"
+    settings.image.path = lamella_directory
 
-        settings.image.path = lamella_directory
+    # Necessary to set imaging settings path
+    acquire.take_reference_images(microscope, settings.image)
 
-        # Necessary to set imaging settings path
-        acquire.take_reference_images(microscope, settings.image)
+    milling_stages = get_milling_stages("mill_polishing", protocol["milling"])
+    assert milling_stages[0].strategy.config == protocol_strategy_config, (
+        "The strategy and protocol configs do not match"
+    )
 
-        milling_stages = get_milling_stages("mill_polishing", protocol["milling"])
-        strategy_config: AdaptivePolishMillingConfig = milling_stages[0].strategy.config
-        print("Adaptive Polishing Config:")
-        print(f"Milling Interval: {strategy_config.milling_interval_s}")
-        print(f"Maximum Cycles: {strategy_config.max_milling_cycles}")
-        print(f"Model: {strategy_config.model_path}")
-
+    with patch.object(
+        microscope,
+        "stop_milling",
+        MagicMock(
+            side_effect=raise_error_after_num_calls(
+                microscope.stop_milling, calls_before_exception=calls_before_exception
+            )
+        ),
+    ) as mock_stop_milling:
         # run milling stages
         mill_stages(microscope=microscope, stages=milling_stages)
 
-        assert mock_stop_milling.call_count == 2, (
+        assert mock_stop_milling.call_count == expected_loops, (
             "stop_milling was called an unexpected number of times"
         )
 
-        # Check directories were created
-        assert adaptive_polish_dir.is_dir(), "adaptive_polish directory wasn't created"
-        for name in ("plots", "sem", "fib"):
-            assert (adaptive_polish_dir / name).is_dir(), (
-                f"{name} subdirectory wasn't created"
-            )
-
-        exp = Experiment.load(experiment_config_path)
-        pos = exp.positions[0]
-        protocol = AutoLamellaProtocol.load(protocol_path)
-        print(protocol.method.workflow)
-
-        print(f"Last Completed: {pos.last_completed}")
-        print(f"Next: {protocol.method.get_next(pos.workflow)}")
-        print(f"Previous: {protocol.method.get_previous(pos.workflow)}")
-        print(f"Workflow: {protocol.method.workflow}")
-
-        acquire.take_reference_images(microscope, settings.image)
-
-        milling_stages = protocol.milling["mill_polishing"]
-        strategy_config: AdaptivePolishMillingConfig = milling_stages[0].strategy.config
-        for k, v in _AP_MILLING_CONFIG_SETTINGS.items():
-            value = getattr(strategy_config, k)
-            assert getattr(strategy_config, k) == v, (
-                f"Strategy config does not match expected value: {k} is {value}, expected {v}"
-            )
-
-        # run milling stages
-        mill_stages(microscope=microscope, stages=milling_stages)
-
-        assert mock_stop_milling.call_count == expected_loops * 2, (
-            "stop_milling was called an unexpected number of times"
+    # Check directories were created
+    assert adaptive_polish_dir.is_dir(), "adaptive_polish directory wasn't created"
+    for name in ("plots", "sem", "fib"):
+        assert (adaptive_polish_dir / name).is_dir(), (
+            f"{name} subdirectory wasn't created"
         )
 
-    expected_stems = [f"lamella_AP_img_{_:>03}_" for _ in range(expected_loops)]
+    expected_fn_stems = [f"lamella_AP_img_{_:>03}_" for _ in range(expected_loops)]
 
     # Check plots exist
     centring_plot_path = adaptive_polish_dir / "centring.png"
@@ -180,7 +157,7 @@ def test_runs(
     plots_dir = adaptive_polish_dir / "plots"
     plot_paths = set(plots_dir.glob("*.png"))
     assert len(plot_paths), "No plots have been created"
-    expected_plot_file_names = (f"{_}plot.png" for _ in expected_stems)
+    expected_plot_file_names = (f"{_}plot.png" for _ in expected_fn_stems)
     expected_plot_paths = set((plots_dir / _ for _ in expected_plot_file_names))
     assert plot_paths == expected_plot_paths, (
         "Expected plot file paths do not match found plot paths"
@@ -201,7 +178,7 @@ def test_runs(
         image_paths = set(image_path.glob("*.tif"))
         assert len(image_paths), f"No {image_type} images have been created"
         expected_plot_file_names = (
-            f"{_}{image_type.upper()}.tif" for _ in expected_stems
+            f"{_}{image_type.upper()}.tif" for _ in expected_fn_stems
         )
         expected_plot_paths = set((image_path / _ for _ in expected_plot_file_names))
         assert image_paths == expected_plot_paths, (
