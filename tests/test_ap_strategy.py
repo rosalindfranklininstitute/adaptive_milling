43742d08b4b050f4361df893f2d7b7dffefa8585
Thomas M Fish <50103643+thomasmfish@users.noreply.github.com>
2025-04-07 20:21:59 +0100

    More strategy unit tests

--- tests/test_ap_strategy.py
+++ tests/test_ap_strategy.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 import pytest
-from unittest.mock import patch, MagicMock, ANY
+from unittest.mock import patch, MagicMock, call, ANY
 
 import typing
 from pathlib import Path
+import numpy as np
+import pandas as pd
 
 from fibsem import utils as fibsem_utils, acquire
 from fibsem.milling.base import get_milling_stages
@@ -25,9 +27,9 @@ if typing.TYPE_CHECKING:
 
 _AP_PASS_CHECKS_CONFIG = {
     "gis_stop_um": -1,
-    "max_crack_area_um2": 1e4,
+    "max_crack_area_um2": np.inf,
     "minimum_lamella_area_um2": 0,
-    "maximum_drift_um": 1e4,
+    "maximum_drift_um": np.inf,
 }
 
 
@@ -289,14 +291,225 @@ def test_milling_stops_when_check_fails(
         mock_draw_patterns.assert_not_called()
 
 
-# def test_max_milling_cycles_not_exceeded() -> None:
-#     """Tests that milling cycles cannot exceed the max"""
-#     pass
+@patch.object(ap_strategy.AdaptivePolishMillingStrategy, "_check_lamella")
+@patch.object(ap_strategy.AdaptivePolishMillingStrategy, "_mill")
+def test_max_milling_cycles_not_exceeded(
+    mock_mill,
+    mock_check_lamella,
+    protocol_template_path: Path,
+    microscope_config_path: Path,
+    fib_image_dir: Path,
+    sem_image_dir: Path,
+    tmp_path: Path,
+) -> None:
+    """Tests that milling cycles cannot exceed the max"""
+    max_milling_cycles = 2
+
+    model_path = "path/to/model.file"
+    pass_checks_kwargs = _AP_PASS_CHECKS_CONFIG.copy()
+    pass_checks_kwargs["max_milling_cycles"] = max_milling_cycles
+
+    ap_config = ap_strategy.AdaptivePolishMillingConfig(
+        model_path=model_path,
+        align_sem=False,
+        **pass_checks_kwargs,
+    )
+    _, stages = setup_protocol_and_milling_stages(
+        ap_config.to_dict(), protocol_template_path, tmp_path
+    )
+    stage = stages[0]
+
+    microscope, settings = setup_microscope_and_settings(
+        microscope_config_path,
+        tmp_path,
+        fib_image_dir=str(fib_image_dir),
+        sem_image_dir=str(sem_image_dir),
+    )
+
+    strategy = ap_strategy.AdaptivePolishMillingStrategy(config=ap_config)
+
+    lamella_directory = tmp_path / "lamella"
+    lamella_directory.mkdir()
+    settings.image.path = lamella_directory
+
+    # Necessary to set imaging settings path
+    acquire.take_reference_images(microscope, settings.image)
+
+    # Stop it trying to load a model
+    with patch.object(strategy, "model"):
+        strategy.run(microscope, stage)
+
+        mock_check_lamella.assert_has_calls(
+            [
+                call(
+                    i,
+                    image_name=f"{lamella_directory.stem}_AP_img_{i:03}",
+                    fib_image=ANY,
+                    sem_image=ANY,
+                    config=strategy.config,
+                    model=strategy.model,
+                    lamella_ap_folder=lamella_directory / "adaptive_polish",
+                    lamella_ap_plots_folder=lamella_directory
+                    / "adaptive_polish"
+                    / "plots",
+                    results=ANY,
+                    gis_results_detailed=ANY,
+                )
+                for i in range(max_milling_cycles)
+            ]
+        )
+
+        mock_mill.assert_has_calls(
+            [
+                call(microscope=microscope, stage=stage, config=strategy.config)
+                for _ in range(max_milling_cycles)
+            ]
+        )
 
 
-# def test_results_saved() -> None:
-#     """Tests that results are saved in correct file naming convention"""
-#     pass
+@patch.object(ap_strategy.gm, "clean_prediction")
+@patch.object(ap_strategy.gm, "filter_gis_thickness")
+@patch.object(ap_strategy.AdaptivePolishMillingStrategy, "_mill")
+def test_results_saved(
+    mock_mill,
+    mock_filter_gis_thickness,
+    mock_clean_prediction,
+    protocol_template_path: Path,
+    microscope_config_path: Path,
+    fib_image_dir: Path,
+    sem_image_dir: Path,
+    tmp_path: Path,
+) -> None:
+    """Tests that results are saved in correct file naming convention"""
+    max_milling_cycles = 3
+
+    model_path = "path/to/model.file"
+    pass_checks_kwargs = _AP_PASS_CHECKS_CONFIG.copy()
+    pass_checks_kwargs["max_milling_cycles"] = max_milling_cycles
+
+    ap_config = ap_strategy.AdaptivePolishMillingConfig(
+        model_path=model_path,
+        align_sem=False,
+        **pass_checks_kwargs,
+    )
+    _, stages = setup_protocol_and_milling_stages(
+        ap_config.to_dict(), protocol_template_path, tmp_path
+    )
+    stage = stages[0]
+
+    microscope, settings = setup_microscope_and_settings(
+        microscope_config_path,
+        tmp_path,
+        fib_image_dir=str(fib_image_dir),
+        sem_image_dir=str(sem_image_dir),
+    )
+
+    strategy = ap_strategy.AdaptivePolishMillingStrategy(config=ap_config)
+
+    lamella_directory = tmp_path / "lamella"
+    lamella_directory.mkdir()
+    settings.image.path = lamella_directory
+    adaptive_polish_dir = lamella_directory / "adaptive_polish"
+
+    # Necessary to set imaging settings path
+    acquire.take_reference_images(microscope, settings.image)
+
+    # Use this resolution to avoid any scaling so know the exact output
+    mask_shape = microscope.electron_system.beam.resolution[::-1]
+    lamella_bottom = mask_shape[0] // 3
+    gis_bottom = mask_shape[0] * 2 // 3
+
+    lamella_mask = np.zeros(mask_shape, dtype=np.bool_)
+    gis_mask = lamella_mask.copy()
+
+    lamella_mask[:lamella_bottom, :] = True
+    gis_mask[lamella_bottom:gis_bottom, :] = True
+
+    mock_clean_prediction.return_value = (lamella_mask, gis_mask, None)
+
+    expected_gis_thickness = np.asarray(
+        [gis_bottom - lamella_bottom] * mask_shape[1], dtype=np.float32
+    )
+    expected_min_gis_thicknesses = [5 * i for i in range(1, max_milling_cycles + 1)]
+    expected_filtered_gis_thicknesses = [
+        np.arange(
+            i,
+            mask_shape[1] + i,
+            dtype=np.uint16,
+        )
+        for i in expected_min_gis_thicknesses
+    ]
+
+    mock_filter_gis_thickness.side_effect = expected_filtered_gis_thicknesses
+    # Stop it trying to load a model
+    with patch.object(strategy, "model"):
+        strategy.run(microscope, stage)
+
+        # # The calls for cracks will be skipped as mask_crack_clean is None
+        # mock_get_mask_area_um2.assert_has_calls(
+        #     [call(lamella_mask, pixel_size_um=ANY) for _ in range(max_milling_cycles)]
+        # )
+        assert mock_filter_gis_thickness.call_count == max_milling_cycles, (
+            f"Milling didn't run the full {max_milling_cycles} times"
+        )
+        mock_mill.assert_has_calls(
+            [
+                call(microscope=microscope, stage=stage, config=strategy.config)
+                for _ in range(max_milling_cycles)
+            ]
+        )
+    # Results (csv)
+    results_path = adaptive_polish_dir / "GIS_thickness.json"
+    detailed_results_path = adaptive_polish_dir / "GIS_thickness_detailed.json"
+    assert results_path.is_file(), "Results file does not exist"
+    assert detailed_results_path.is_file(), "Detailed results file does not exist"
+
+    images_names = [
+        f"{lamella_directory.stem}_AP_img_{i:03}" for i in range(max_milling_cycles)
+    ]
+    milling_times = [
+        ap_config.milling_interval_s * (i + 1) for i in range(max_milling_cycles)
+    ]
+
+    expected_results_df = pd.DataFrame(
+        {
+            "image": images_names,
+            "milling_time_s": milling_times,
+            "min_GIS_um": expected_min_gis_thicknesses,
+            "crack_area_um2": [0] * max_milling_cycles,
+        }
+    )
+
+    # results_df = pd.read_csv(results_path, index_col=0)
+    results_df = pd.read_json(results_path)
+    pd.testing.assert_frame_equal(
+        results_df, expected_results_df, check_dtype=False, obj="Results DataFrame"
+    )
+    expected_detailed_results_df = pd.DataFrame(
+        {
+            "image": images_names,
+            "milling_time_s": milling_times,
+            "gis_thickness_um": pd.Series(
+                [expected_gis_thickness.tolist()] * max_milling_cycles, dtype=object
+            ),
+            "gis_thickness_filtered_um": pd.Series(
+                [_.tolist() for _ in expected_filtered_gis_thicknesses], dtype=object
+            ),
+        }
+    )
+
+    detailed_results_df = pd.read_csv(detailed_results_path, index_col=0)
+    detailed_results_df = pd.read_json(detailed_results_path)
+    first_gis_thicknesses = detailed_results_df.loc[0, "gis_thickness_um"]
+    assert len(first_gis_thicknesses) == mask_shape[1], (
+        "Unexpected length of gis_thickness_um"
+    )
+    pd.testing.assert_frame_equal(
+        detailed_results_df,
+        expected_detailed_results_df,
+        check_dtype=False,
+        obj="Detailed results DataFrame",
+    )
 
 
 # def test_milling_time_adjustment() -> None:
