52d499f056826fc3ae97b34b62b431b43b99bfab
Elaine Ho <elaine.ho@rfi.ac.uk>
2025-04-25 15:09:20 +0100

    Set y limits of GIS thickness plot

--- src/adaptive_polish/plot.py
+++ src/adaptive_polish/plot.py
@@ -188,6 +188,7 @@ def create_milling_cycle_plot(
     axs[1, 2].set_xlim(0, len(gis_thickness_um))
     axs[1, 2].set_ylim(
         0,
+        gis_stop_um * 1.1
     )
     axs[1, 2].hlines(
         y=gis_stop_um,
