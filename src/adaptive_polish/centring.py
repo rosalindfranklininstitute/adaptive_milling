5809447ae29528afcca384af4cd59476a40b55f8
Thomas M Fish <50103643+thomasmfish@users.noreply.github.com>
2025-06-19 13:51:23 +0100

    Split up get_bounding_box_from_edges
    Added new tests

--- src/adaptive_polish/centring.py
+++ src/adaptive_polish/centring.py
@@ -10,11 +10,56 @@ from adaptive_polish.exceptions import CentringException
 
 if typing.TYPE_CHECKING:
     from numpy.typing import NDArray
-    from collections.abc import Callable
+    from collections.abc import Callable, Sequence
 
 
-def get_mask_bounding_box(
+def get_mask_edge(
+    mask_2d: NDArray[np.bool_], axis: int, side: typing.Literal["min", "max"] = "min"
+) -> NDArray[np.uint16]:
+    if side == "max":
+        indexes = (
+            slice(None, None, -1 if axis == 0 else None),
+            slice(None, None, -1 if axis == 1 else None),
+        )
+        edge = (
+            mask_2d.shape[axis]
+            - np.argmax(mask_2d[indexes[0], indexes[1]], axis=axis)
+            - 1
+        )
+    elif side == "min":
+        edge = np.argmax(mask_2d, axis=axis)
+    else:
+        raise ValueError(f"Invalid argument for side '{side}'")
+
+    other_axis_range = np.arange(mask_2d.shape[1 - axis], dtype=edge.dtype)
+
+    coordinates = np.stack([edge, other_axis_range], axis=-1)
+    if axis != 0:
+        coordinates = coordinates[:, ::-1]
+
+    # Only include edge values that are True
+    # This filters out any rows/columns that were all False
+    valid_idxs = mask_2d[coordinates[:, 0], coordinates[:, 1]]
+    return coordinates[valid_idxs].astype(np.uint16)
+
+
+def get_mask_edges(
     mask: NDArray[np.bool_],
+) -> typing.List[typing.List[NDArray[np.uint16]]]:
+    return [
+        [
+            get_mask_edge(mask, axis=0, side="min"),
+            get_mask_edge(mask, axis=0, side="max"),
+        ],
+        [
+            get_mask_edge(mask, axis=1, side="min"),
+            get_mask_edge(mask, axis=1, side="max"),
+        ],
+    ]
+
+
+def get_bounding_box_from_edges(
+    edges_array: Sequence[Sequence[NDArray[np.integer[typing.Any]]]],
     edge_finding: typing.Literal["median", "mean", "min", "max"] = "median",
 ) -> typing.Tuple[float, float, float, float]:
     edge_fn_max: Callable[..., np.integer[typing.Any] | np.floating[typing.Any]]
@@ -33,28 +78,22 @@ def get_mask_bounding_box(
         edge_fn_max = np.max
     else:
         raise ValueError(f"Invalid option edge_finding='{edge_finding}'")
-
-    x_mins = np.argmax(mask, axis=1)
-    x_maxs = mask.shape[1] - np.argmax(mask[:, ::-1], axis=1) - 1
-    y_mins = np.argmax(mask, axis=0)
-    y_maxs = mask.shape[0] - np.argmax(mask[::-1, :], axis=0) - 1
-    x_range = np.arange(mask.shape[1])
-    y_range = np.arange(mask.shape[0])
-    # Only include edge values that are True
-    # This filters out any rows/columns that were all False
-    valid_xmins = x_mins[mask[y_range, x_mins]]
-    valid_xmaxs = x_maxs[mask[y_range, x_maxs]]
-    valid_ymins = y_mins[mask[y_mins, x_range]]
-    valid_ymaxs = y_maxs[mask[y_maxs, x_range]]
-    xmin = float(edge_fn_min(valid_xmins))
-    xmax = float(edge_fn_max(valid_xmaxs))
-    ymin = float(edge_fn_min(valid_ymins))
-    ymax = float(edge_fn_max(valid_ymaxs))
+    xmin = edge_fn_min(edges_array[1][0][:, 1]).item()
+    xmax = edge_fn_max(edges_array[1][1][:, 1]).item()
+    ymin = edge_fn_min(edges_array[0][0][:, 0]).item()
+    ymax = edge_fn_max(edges_array[0][1][:, 0]).item()
     bbox = (ymin, xmin, ymax, xmax)
 
     if np.any(np.isnan(bbox)):
         raise CentringException("Bounding box coordinates contains a NaN")
     return bbox
+
+
+def get_mask_bounding_box(
+    mask: NDArray[np.bool_],
+    edge_finding: typing.Literal["median", "mean", "min", "max"] = "median",
+) -> typing.Tuple[float, float, float, float]:
+    return get_bounding_box_from_edges(get_mask_edges(mask), edge_finding=edge_finding)
 
 
 def get_centre_from_bounding_box(
@@ -76,7 +115,9 @@ def get_lamella_centre(
     edge_finding: typing.Literal["median", "mean"] = "median",
     subpixel_accuracy: bool = False,
 ) -> typing.Union[typing.Tuple[int, int], typing.Tuple[float, float]]:
-    bbox = get_mask_bounding_box(array, edge_finding=edge_finding)
+    bbox = get_bounding_box_from_edges(
+        get_mask_edges(mask=array), edge_finding=edge_finding
+    )
     return get_centre_from_bounding_box(bbox, subpixel_accuracy=subpixel_accuracy)
 
 
--- tests/test_centring.py
+++ tests/test_centring.py
@@ -1,4 +1,6 @@
+from __future__ import annotations
 import pytest
+from numpy.testing import assert_array_equal
 
 from typing import Literal
 
@@ -8,9 +10,101 @@ from fibsem.detection.detection import AdaptiveLamellaCentre
 from adaptive_polish.centring import (
     get_mask_bounding_box,
     get_lamella_centre,
+    get_mask_edges,
+    get_bounding_box_from_edges,
+    get_centre_from_bounding_box,
 )
 
 from .setup import SimpleRectangleLamellaMask
+
+
+def test_get_mask_edges() -> None:
+    test_lamella = SimpleRectangleLamellaMask((100, 200), 20)
+    edges = get_mask_edges(test_lamella.array)
+    y_range = np.arange(test_lamella.bounding_box[0], test_lamella.bounding_box[2] + 1)
+    x_range = np.arange(test_lamella.bounding_box[1], test_lamella.bounding_box[3] + 1)
+    expected_edges = [
+        [
+            np.stack(
+                ([test_lamella.bounding_box[0]] * len(x_range), x_range), axis=-1
+            ).astype(np.uint16),
+            np.stack(
+                ([test_lamella.bounding_box[2]] * len(x_range), x_range), axis=-1
+            ).astype(np.uint16),
+        ],
+        [
+            np.stack(
+                (y_range, [test_lamella.bounding_box[1]] * len(y_range)), axis=-1
+            ).astype(np.uint16),
+            np.stack(
+                (y_range, [test_lamella.bounding_box[3]] * len(y_range)), axis=-1
+            ).astype(np.uint16),
+        ],
+    ]
+    for i, axis in enumerate(edges):
+        for j, edge in enumerate(axis):
+            assert_array_equal(
+                edge,
+                expected_edges[i][j],
+                err_msg=f"Edge {['X', 'Y'][i]} {['min', 'max'][j]} do not match",
+            )
+
+
+@pytest.mark.parametrize("edge_finding", ["median", "mean", "min", "max"])
+def test_get_bounding_box_from_edges(
+    edge_finding: Literal["median", "mean", "min", "max"],
+) -> None:
+    expected_bounding_box = np.asarray(
+        (
+            50,
+            20,
+            70,
+            60,
+        )
+    )
+    y_range = np.arange(expected_bounding_box[0], expected_bounding_box[2] + 1)
+    x_range = np.arange(expected_bounding_box[1], expected_bounding_box[3] + 1)
+    edges = [
+        [
+            np.stack(
+                ([expected_bounding_box[0]] * len(x_range), x_range), axis=-1
+            ).astype(np.uint16),
+            np.stack(
+                ([expected_bounding_box[2]] * len(x_range), x_range), axis=-1
+            ).astype(np.uint16),
+        ],
+        [
+            np.stack(
+                (y_range, [expected_bounding_box[1]] * len(y_range)), axis=-1
+            ).astype(np.uint16),
+            np.stack(
+                (y_range, [expected_bounding_box[3]] * len(y_range)), axis=-1
+            ).astype(np.uint16),
+        ],
+    ]
+    bounding_box = get_bounding_box_from_edges(edges, edge_finding=edge_finding)
+    assert_array_equal(
+        bounding_box, expected_bounding_box, err_msg="Bounding box is not as expected"
+    )
+
+
+@pytest.mark.parametrize("subpixel_accuracy", [True, False], ids=["subpixel", "pixel"])
+def test_get_centre_from_bounding_box(subpixel_accuracy: bool):
+    expected_centre = np.asarray((20.5, 50.5))
+    diffs = np.asarray((5.5, 12.5))
+    if not subpixel_accuracy:
+        expected_centre = expected_centre.astype(np.uint8)
+        diffs = diffs.astype(np.uint8)
+
+    bounding_box = np.concatenate((expected_centre - diffs, expected_centre + diffs))
+    assert_array_equal(
+        get_centre_from_bounding_box(
+            (bounding_box[0], bounding_box[1], bounding_box[2], bounding_box[3]),
+            subpixel_accuracy=subpixel_accuracy,
+        ),
+        expected_centre,
+        err_msg="Failed to find correct centre",
+    )
 
 
 def test_methods_equivalent_for_simple_rectangle() -> None:
