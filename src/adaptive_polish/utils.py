875ebd0d72adcaafac3b0b26b2181c7d8810a705
Thomas M Fish <50103643+thomasmfish@users.noreply.github.com>
2025-07-23 15:19:34 +0100

    formatting
    formatting
    
    formatting

--- src/adaptive_polish/utils.py
+++ src/adaptive_polish/utils.py
@@ -75,6 +75,7 @@ def ensure_subdirectories(directory: Path, *subdirectory_names: str) -> list[Pat
         directory.mkdir(exist_ok=True)
     return directories
 
+
 def setup_results_df() -> tuple[pd.DataFrame, pd.DataFrame]:
     results = pd.DataFrame(
         {
