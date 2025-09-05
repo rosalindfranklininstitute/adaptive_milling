eebba9068d02c2234e6a522d53534e87e836ce08
Thomas M Fish <50103643+thomasmfish@users.noreply.github.com>
2025-09-05 11:20:11 +0100

    Initial draft of bitmap strategy and config (pending FIBSEM updates)

--- src/adaptive_polish/config/__init__.py
+++ src/adaptive_polish/config/__init__.py
@@ -1,5 +1,6 @@
 import typing
 from adaptive_polish.config.adaptive_polish import AdaptivePolishMillingConfig
+from adaptive_polish.config.bitmap import BitmapAdaptivePolishMillingConfig
 
 TAdaptivePolishMillingConfig = typing.TypeVar(
     "TAdaptivePolishMillingConfig", bound=AdaptivePolishMillingConfig
@@ -9,4 +10,5 @@ TAdaptivePolishMillingConfig = typing.TypeVar(
 __all__ = [
     "TAdaptivePolishMillingConfig",
     "AdaptivePolishMillingConfig",
+    "BitmapAdaptivePolishMillingConfig",
 ]
