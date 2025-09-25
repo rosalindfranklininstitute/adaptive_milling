c1d0d1726fd3939f0ec9bcc6cf38de5e94ff863b
Thomas Fish <50103643+thomasmfish@users.noreply.github.com>
2025-09-25 14:33:37 +0100

    New pattern centring method `crop_xlims_convolve_filtered` (#41)
    * New pattern centring method crop_xlims_convolve_filtered
    
    * pattern_centring.py: include crop_xlims_convolve_filtered

--- src/adaptive_polish/processing/lamella.py
+++ src/adaptive_polish/processing/lamella.py
@@ -18,7 +18,7 @@ from math import ceil, floor
 
 import numpy as np
 from scipy.signal.windows import gaussian
-from scipy.ndimage import gaussian_filter1d
+from scipy.ndimage import median_filter, gaussian_filter1d
 
 from adaptive_polish.processing.image import (
     resize_image,
@@ -349,12 +349,33 @@ def crop_xlims_convolve(
     return (xlims[0] + min_diffs[0], xlims[0] + min_diffs[1])
 
 
+def crop_xlims_convolve_filtered(
+    gis_thickness: NDArray[np.float32 | np.float64],
+    xlims: tuple[int, int],
+    lamella_width: int,
+    edge_size: int = 15,
+    sigma: float = 0.8,
+    filter_size: int = 136,
+) -> tuple[int, int]:
+    trimmed_thickness = gis_thickness[xlims[0] : xlims[1] + 1]
+    trimmed_filtered_thickness = median_filter(gis_thickness, size=filter_size)[
+        xlims[0] : xlims[1] + 1
+    ]
+    min_diffs = _get_milled_area_convolve_gaussian(
+        np.log(trimmed_thickness / trimmed_filtered_thickness),
+        width=lamella_width,
+        edge_size=edge_size,
+        sigma=sigma,
+    )
+    return (xlims[0] + min_diffs[0], xlims[0] + min_diffs[1])
+
+
 def _get_milled_area_convolve_gaussian(
     data: NDArray[np.float32 | np.float64],
     width: int,
     edge_size: int = 17,
     sigma: float = 0.7,
-):
+) -> tuple[int, int]:
     kernel = np.zeros((width,))
     if edge_size <= 0:
         raise ValueError("edge_size must be above 0")
