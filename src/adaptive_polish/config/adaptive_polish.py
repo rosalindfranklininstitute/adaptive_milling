88a825e4d40a9c0132f261dd56e5c0957d0cee8c
Thomas M Fish <50103643+thomasmfish@users.noreply.github.com>
2025-10-17 15:55:42 +0100

    Reorder AdaptivePolishMillingConfig entries Set some fields as advanced attributes Add title and description to lamella_pad_x Field

--- src/adaptive_polish/config/adaptive_polish.py
+++ src/adaptive_polish/config/adaptive_polish.py
@@ -14,18 +14,31 @@ DEFAULT_MODEL_GENERATION = get_latest_generation_key()
 @dataclass(config=ConfigDict(validate_assignment=True))
 class AdaptivePolishMillingConfig(MillingStrategyConfig):
     model_path: str = ""
-    align_sem: bool = True
-    gis_filter_sigma: NonNegativeFloat = 10 / 6
+    model_generation: str = DEFAULT_MODEL_GENERATION
     gis_stop_um: NonNegativeFloat = 0.2
     max_crack_area_um2: NonNegativeFloat = 2
     max_milling_cycles: PositiveInt = 30
     minimum_lamella_area_um2: NonNegativeFloat = 30.0  # 30μm²
     maximum_drift_um: NonNegativeFloat = 70
-    lamella_pad_x: float = Field(  # fraction of lamella width to pad by on each side
-        default=0.0, ge=0, le=1
-    )
-    model_generation: str = DEFAULT_MODEL_GENERATION
+    align_sem: bool = True
     save_predictions: bool = True
+    gis_filter_sigma: NonNegativeFloat = 10 / 6
+    lamella_pad_x: float = Field(
+        default=0.0,
+        ge=0,
+        le=1,
+        title="X pad fraction",
+        description="The fraction of the lamella width that the x limits will "
+        "be padded by. These limits are used to determine the region that GIS "
+        "thickness is measured.",
+    )
+
+    _advanced_attributes: typing.ClassVar[tuple[str, ...]] = (
+        "save_predictions",
+        "gis_filter_sigma",
+        "lamella_pad_x",
+        "save_predictions",
+    )
 
     @field_validator("model_path", mode="after")
     @classmethod
