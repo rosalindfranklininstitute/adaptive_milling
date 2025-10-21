c98c4e77804df1015029748b68416348dbbf08fb
Thomas M Fish <50103643+thomasmfish@users.noreply.github.com>
2025-10-21 11:26:58 +0100

    Stop depending on FiducialPattern and inherit straight from BasePattern Also sets defaults based on what we have been using

--- src/adaptive_polish/patterns.py
+++ src/adaptive_polish/patterns.py
@@ -3,12 +3,18 @@ import math
 import typing
 from dataclasses import dataclass
 
-from fibsem.milling.patterning import FiducialPattern
-from fibsem.structures import FibsemRectangleSettings
+from fibsem.milling.patterning import BasePattern
+from fibsem.structures import FibsemRectangleSettings, CrossSectionPattern
 
 
 @dataclass
-class AsymmetricFiducialPattern(FiducialPattern):
+class AsymmetricFiducialPattern(BasePattern[FibsemRectangleSettings]):
+    width: float = 0.1e-6
+    height: float = 10.0e-6
+    depth: float = 0.5e-6
+    rotation: float = 45.0
+    cross_section: CrossSectionPattern = CrossSectionPattern.Rectangle
+
     name: typing.ClassVar[str] = "AsymmetricFiducial"
 
     def define(self) -> list[FibsemRectangleSettings]:
