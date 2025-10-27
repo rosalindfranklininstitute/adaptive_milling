3b3334358645af1ddbd90027e1f4d9213e0747d8
Thomas M Fish <50103643+thomasmfish@users.noreply.github.com>
2025-10-27 16:46:15 +0000

    Fix create_summary_gis_plot

--- src/adaptive_polish/plot.py
+++ src/adaptive_polish/plot.py
@@ -339,7 +339,7 @@ def create_summary_gis_plot(
     )
     ax.plot(
         cumulative_milling_time,
-        np.asarray(median_gis_thicknesses, dtype=np.float_),
+        np.asarray(mean_gis_thicknesses, dtype=np.float_),
         label="Mean",
         linestyle=":",
     )
