f89645e7421397a7e09ef865ea2e15a0a2848274
Thomas M Fish <50103643+thomasmfish@users.noreply.github.com>
2025-10-27 15:55:22 +0000

    Add bitmap _advanced_attributes

--- src/adaptive_polish/config/bitmap.py
+++ src/adaptive_polish/config/bitmap.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Literal
+from typing import Literal, ClassVar
 from pydantic import (
     ConfigDict,
     NonNegativeInt,
@@ -36,3 +36,9 @@ class BitmapAdaptivePolishMillingConfig(AdaptivePolishMillingConfig):
     apply_boundary_smoothing: bool = False
     mask_cracks: bool = True
     pattern_alignment: Literal["centre", "convolve"] = "centre"
+
+    _advanced_attributes: ClassVar[tuple[str, ...]] = (
+        *AdaptivePolishMillingConfig._advanced_attributes,
+        "save_predictions",
+        "pattern_alignment",
+    )
