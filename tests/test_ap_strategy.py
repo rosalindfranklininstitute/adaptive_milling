326c7d246ccb94bb7762844595dfaac35634ee4c
Thomas M Fish <50103643+thomasmfish@users.noreply.github.com>
2025-10-28 18:30:35 +0000

    Fix test to handle estimated milling times

--- tests/test_ap_strategy.py
+++ tests/test_ap_strategy.py
@@ -509,6 +509,8 @@ def test_results_saved(
         patch.object(strategy, "_update_milling_stage") as mock_update_milling_stage,
         patch.object(strategy, "_mill") as mock_mill,
     ):
+        estimated_milling_times_s = np.linspace(10, 50, max_checks).tolist()
+        mock_mill.side_effect = estimated_milling_times_s
         mock_model.predict.return_value = prediction
         mock_update_milling_stage.return_value = deepcopy(stage)
 
@@ -557,7 +559,7 @@ def test_results_saved(
                     "crack_count": 0,
                     "lamella_thickness_prediction_px": ANY,
                     "crack_thickness_prediction_px": ANY,
-                    "estimated_milling_time_s": ANY,
+                    "estimated_milling_time_s": estimated_milling_times_s[i],
                     "lamella_bounding_box_prediction_px": ANY,
                     "xlims_prediction_px": ANY,
                     "lamella_bounding_box_image_px": ANY,
