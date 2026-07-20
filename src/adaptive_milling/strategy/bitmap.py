20b676602acf3f27b2720c517b71ba5537110876
Thomas M Fish <50103643+thomasmfish@users.noreply.github.com>
2026-07-20 17:49:07 +0100

    Set bitmap interpolation to bilinear

--- src/adaptive_milling/strategy/bitmap.py
+++ src/adaptive_milling/strategy/bitmap.py
@@ -158,7 +158,7 @@ class BitmapAdaptivePolishMillingStrategy(
             lower_trench_height=pattern.lower_trench_height,
             time=pattern.time,
             array=bitmap_array,
-            interpolation="bicubic",
+            interpolation="bilinear",
         )
 
     def _convert_rectangle_pattern(
@@ -194,7 +194,7 @@ class BitmapAdaptivePolishMillingStrategy(
             passes=pattern.passes,
             scan_direction=pattern.scan_direction,
             array=bitmap_array,
-            interpolation="bicubic",
+            interpolation="bilinear",
         )
 
     @staticmethod
