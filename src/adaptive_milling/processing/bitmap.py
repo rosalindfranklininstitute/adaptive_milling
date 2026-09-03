668dad1196185f00e68bd7bd3c3c006277c893e8
Thomas M Fish <50103643+thomasmfish@users.noreply.github.com>
2026-09-03 12:31:39 +0100

    Revert "Remove BitmapAdaptivePolish"
    This reverts commit 1730190a2d4182c03c8ee87e00e2a3952702d165.

---
+++ src/adaptive_milling/processing/bitmap.py
@@ -0,0 +1,263 @@
+# general
+from __future__ import annotations
+
+import typing
+
+import numpy as np
+from scipy import ndimage as ndi
+
+if typing.TYPE_CHECKING:
+    from numpy.typing import NDArray
+
+
+def create_dwell_and_blanking_arrays(
+    input_signal: NDArray[
+        np.float128 | np.float96 | np.float64 | np.float32 | np.float16
+    ],
+    min_dwell_threshold: float,
+    max_dwell_threshold: float,
+    blanking_threshold: float | None = None,
+    input_resolution_m: float | None = None,
+    blanking_width_m: float | None = None,
+    bitmap_resolution_m: float | None = None,
+    max_output_range: tuple[float, float] = (0, 255),
+    nan_value: float = 0,
+) -> tuple[NDArray[typing.Any], NDArray[np.bool_] | None]:
+    """
+    If `bitmap_resolution_m` and `input_resolution_m` are `None`, no pixel interpolation is used.
+
+    `blanking_width_m` is only used if both `blanking_threshold` and `bitmap_resolution_m` are not `None`
+    """
+
+    n = len(input_signal)
+    if input_resolution_m is not None and bitmap_resolution_m is not None:
+        factor = bitmap_resolution_m / input_resolution_m
+    else:
+        factor = 1
+
+    x = np.linspace(0, n - 1, round((n - 1) * factor) + 1)
+    input_signal = input_signal.copy()
+    input_signal[np.isnan(input_signal)] = nan_value
+    # Interpolate over to ensure pixel size
+    interpolated = np.interp(x, range(n), input_signal)
+
+    # Rescale values to from (min_dwell_threshold, max_dwell_threshold) to max_output_range
+    rescaled = np.interp(
+        interpolated,
+        (min_dwell_threshold, max_dwell_threshold),
+        max_output_range,
+    ).reshape(1, -1)
+
+    if blanking_threshold is not None:
+        blanking_array = (interpolated < blanking_threshold).reshape(1, -1)
+        if blanking_width_m is not None and bitmap_resolution_m is not None:
+            blanking_half_width_px = int(
+                np.ceil(blanking_width_m / (2 * bitmap_resolution_m))
+            )
+            ndi.binary_dilation(
+                blanking_array,
+                iterations=blanking_half_width_px - 1,
+                output=blanking_array,
+            )
+
+    else:
+        # Don't blank anything
+        blanking_array = None
+
+    return rescaled, blanking_array
+
+
+def bitmap_to_points(bitmap_image: NDArray[np.uint8]) -> NDArray[typing.Any]:
+    points_array = np.empty((*bitmap_image.shape[:2], 2), dtype=object)
+    points_array[:, :, 0] = np.interp(bitmap_image[:, :, 2], (0, 255), (0, 1))
+    points_array[:, :, 1] = 1 - bitmap_image[:, :, 1]
+    return points_array
+
+
+@typing.overload
+def create_bitmap_array(
+    input_signal: NDArray[
+        np.float128 | np.float96 | np.float64 | np.float32 | np.float16
+    ],
+    xlims: tuple[int, int],
+    max_dwell_threshold: float,
+    min_dwell_threshold: float,
+    input_resolution_m: float | None = ...,
+    bitmap_resolution_m: float | None = ...,
+    blanking_threshold: float | None = ...,
+    blanking_width_m: float = ...,
+    nan_value: float = ...,
+    as_image: typing.Literal[True] = ...,
+) -> NDArray[np.uint8]: ...
+
+
+@typing.overload
+def create_bitmap_array(
+    input_signal: NDArray[
+        np.float128 | np.float96 | np.float64 | np.float32 | np.float16
+    ],
+    xlims: tuple[int, int],
+    max_dwell_threshold: float,
+    min_dwell_threshold: float,
+    input_resolution_m: float | None = ...,
+    bitmap_resolution_m: float | None = ...,
+    blanking_threshold: float | None = ...,
+    blanking_width_m: float = ...,
+    nan_value: float = ...,
+    as_image: typing.Literal[False] = ...,
+) -> NDArray[typing.Any]: ...
+
+
+def create_bitmap_array(
+    input_signal: NDArray[
+        np.float128 | np.float96 | np.float64 | np.float32 | np.float16
+    ],
+    xlims: tuple[int, int],
+    max_dwell_threshold: float,
+    min_dwell_threshold: float,
+    input_resolution_m: float | None = None,
+    bitmap_resolution_m: float | None = None,
+    blanking_threshold: float | None = None,  # Blanking won't be applied
+    blanking_width_m: float = 5e-7,
+    nan_value: float = 0,
+    as_image: bool = True,
+) -> NDArray[np.uint8 | typing.Any]:
+    """
+    Creates bitmap array for TFS AutoScript API.
+    This has two behaviours:
+
+    - Bitmap image (for loading with `BitmapPatternDefinition.load(bitmap_path)`)
+        - 3-channel (RGB) images of type `np.uint8` in the shape (y, x, c)
+        - Channel 0 (R) is not used
+        - Channel 1 (G) is a flag (0 blanks the point, 1 means no flags)
+        - Channel 2 (B) is the dwell time multiplier (255 translates to 1x the pattern value)
+
+    - Bitmap points (for setting a numpy array to `bpd = BitmapPatternDefinition(); bpd.points = bitmap_array`)
+        - 2-channel numpy arrays of type `object` in the shape (y, x, c)
+        - Channel 0 is the dwell time multiplier (now a float with the range of 0-1)
+        - Channel 1 is a flag (0 means no flags, 1 blanks the point)
+        - There is no Channel 2 (this was channel 0)
+
+    The key changes, which are internally made by `BitmapPatternDefinition.load(bitmap_path)`, are:
+    - The channels are flipped around.
+    - The dwell time are floats between 0 and 1.
+    - The flag values have been inverted.
+    - What was previously Channel 0 has been removed.
+    """
+    if as_image:
+        dwell_time_range = (0, 255)
+        dwell_time_channel = 2
+        blanking_flag_value = 0
+        bitmap_array = (
+            np.ones(  # If flags channel were all 0s, it would blank everything
+                (
+                    1,  # This could be expanded if some falloff is wanted
+                    xlims[1] - xlims[0] + 1,
+                    3,
+                ),
+                dtype=np.uint8,
+            )
+        )
+    else:
+        # Pixel value of 0 means as little milling as possible
+        dwell_time_range = (0, 1)
+        dwell_time_channel = 0
+        blanking_flag_value = 1
+        bitmap_array = np.zeros(
+            (
+                1,  # This could be expanded if some falloff is wanted
+                xlims[1] - xlims[0] + 1,
+                2,
+            ),
+            dtype=object,
+        )
+
+    dwell_time_array, blanking_array = create_dwell_and_blanking_arrays(
+        input_signal=input_signal[xlims[0] : xlims[1] + 1],
+        min_dwell_threshold=min_dwell_threshold,
+        max_dwell_threshold=max_dwell_threshold,
+        blanking_threshold=blanking_threshold,
+        max_output_range=dwell_time_range,
+        input_resolution_m=input_resolution_m,
+        bitmap_resolution_m=bitmap_resolution_m,
+        blanking_width_m=blanking_width_m,
+        nan_value=nan_value,
+    )
+    bitmap_array[:, :, dwell_time_channel] = dwell_time_array
+
+    if blanking_array is not None:
+        # Flags are channel 1 for both types
+        bitmap_array[:, :, 1][blanking_array] = blanking_flag_value
+
+    return bitmap_array
+
+
+def filter_bitmap_signal(
+    array_1d: NDArray[np.float32 | np.float64],
+    erosion_px: int,
+    gaussian_sigma: float,
+    minimum_value: float | None = None,
+    maximum_value: float | None = None,
+) -> NDArray[np.float32 | np.float64]:
+    array_1d = np.clip(array_1d, 0, maximum_value)
+
+    if minimum_value is not None:
+        min_mask = array_1d <= minimum_value
+
+        if erosion_px > 0:
+            # Expand the minimum regions according to bitmap_erosion_px and ensure
+            # that the blur doesn't cut into them
+            expanded_mask = ndi.binary_dilation(
+                min_mask,
+                iterations=erosion_px,
+            )
+        else:
+            expanded_mask = min_mask
+    else:
+        expanded_mask = False
+
+    # Use grey erosion to filter
+    array_1d = ndi.grey_erosion(array_1d, size=erosion_px)
+
+    if gaussian_sigma > 0:
+        blurred = ndi.gaussian_filter1d(
+            array_1d, sigma=gaussian_sigma, mode="constant", cval=0, truncate=6
+        )
+        array_1d = blurred
+
+    # Set to 0 to ensure the masked area is definitely at/below the minimum
+    array_1d[expanded_mask] = 0
+
+    return array_1d
+
+
+def get_angle_dwell_multiplier(
+    gis_lower_edge_coordinates: NDArray[
+        np.float64 | np.float32 | np.integer[typing.Any]
+    ],
+    pixel_size: tuple[float, float],
+    sputter_ratio: float = 4.3,
+) -> NDArray[np.float64]:
+    """Create a multiplier to adjust the bitmap signal with.
+
+    This is based on the sputter yield distribution in Fig. 7 of https://doi.org/10.1016/j.matdes.2022.110563
+
+    sputter_ratio sets the ratio between 0 degrees and the maximum sputter rate (at ~80 degrees).
+    """
+    gradient = np.gradient(
+        gis_lower_edge_coordinates[:, 0] * pixel_size[0],
+        gis_lower_edge_coordinates[:, 1] * pixel_size[1],
+    )
+
+    incident_angles = np.arctan(gradient)
+
+    # Yield multiplier is based on fitting a Beta distribution to match
+    # Fig. 7 of https://doi.org/10.1016/j.matdes.2022.110563
+    A = 7
+    p = 5.4
+    q = 1.65
+    yield_multiplier = 1 + sputter_ratio * A * (
+        np.abs(incident_angles) * 2 / np.pi
+    ) ** (p - 1) * (1 - np.abs(incident_angles) * 2 / np.pi) ** (q - 1)
+
+    return 1 / yield_multiplier
---
+++ src/adaptive_milling/strategy/bitmap.py
@@ -0,0 +1,405 @@
+from __future__ import annotations
+
+import logging
+from typing import TYPE_CHECKING
+
+import numpy as np
+from fibsem.milling.patterning import (
+    BitmapPattern,
+    RectanglePattern,
+    TrenchBitmapPattern,
+    TrenchPattern,
+)
+
+from adaptive_milling.config import BitmapAdaptivePolishMillingConfig
+from adaptive_milling.enums import StopReasons
+from adaptive_milling.exceptions import StopMillingException
+from adaptive_milling.plot import create_milling_cycle_plot
+from adaptive_milling.processing.bitmap import (
+    create_bitmap_array,
+    filter_bitmap_signal,
+    get_angle_dwell_multiplier,
+)
+from adaptive_milling.processing.image import get_mask_edge, resize_interp_1d
+from adaptive_milling.processing.segmentation import (
+    SEMSegmentationLabels,
+    crop_xlims_centre,
+    crop_xlims_convolve_filtered,
+    get_lamella_gis_boundary_peturbations,
+)
+from adaptive_milling.strategy import AdaptivePolishMillingStrategy
+
+if TYPE_CHECKING:
+    from pathlib import Path
+    from typing import Any
+
+    from fibsem.milling import FibsemMillingStage
+    from numpy.typing import NDArray
+
+    from adaptive_milling._dataclasses import (
+        LamellaInformation,
+        LamellaStatistics,
+    )
+
+_logger = logging.getLogger(__name__)
+
+
+class BitmapAdaptivePolishMillingStrategy(
+    AdaptivePolishMillingStrategy[BitmapAdaptivePolishMillingConfig]
+):
+    name: str = "BitmapAdaptivePolishing"
+    fullname: str = "Adaptive polishing using bitmap milling"
+    config_class: type[BitmapAdaptivePolishMillingConfig] = (
+        BitmapAdaptivePolishMillingConfig
+    )
+
+    def _update_milling_stage(
+        self,
+        stage: FibsemMillingStage,
+        lamella_info: LamellaInformation,
+    ) -> FibsemMillingStage:
+        fib_image = lamella_info.fib_image
+        sem_image = lamella_info.sem_image
+        stats = lamella_info.statistics
+
+        if fib_image.metadata is None:
+            raise ValueError("FIB image has no metadata")
+        elif sem_image.metadata is None:
+            raise ValueError("SEM image has no metadata")
+
+        stage = super()._update_milling_stage(stage=stage, lamella_info=lamella_info)
+
+        pattern = stage.pattern
+
+        if (
+            self.config.apply_boundary_smoothing
+            and self.config.boundary_smoothing_sigma > 0
+        ):
+            boundary_peturbations = get_lamella_gis_boundary_peturbations(
+                lamella_info.clean_prediction,
+                sigma=self.config.boundary_smoothing_sigma,
+            )
+        else:
+            boundary_peturbations = None
+
+        if self.config.incident_angle_sputter_ratio > 1:
+            gis_mask = lamella_info.clean_prediction == SEMSegmentationLabels.GIS.value
+            gis_lower_edge_coordinates = get_mask_edge(gis_mask, axis=0, side="max")
+        else:
+            gis_lower_edge_coordinates = None
+
+        if isinstance(pattern, TrenchPattern):
+            new_pattern = self._convert_trench_pattern(
+                pattern,
+                stats=stats,
+                lamella_gis_boundary_peturbations=boundary_peturbations,
+                gis_lower_edge_coordinates=gis_lower_edge_coordinates,
+            )
+        elif isinstance(pattern, RectanglePattern):
+            new_pattern = self._convert_rectangle_pattern(
+                pattern,
+                stats=stats,
+                lamella_gis_boundary_peturbations=boundary_peturbations,
+                gis_lower_edge_coordinates=gis_lower_edge_coordinates,
+            )
+        else:
+            raise TypeError(
+                f"Invalid pattern type {pattern.name}, only {TrenchPattern.name} and {RectanglePattern.name} are supported"
+            )
+
+        stage.pattern = new_pattern
+
+        _logger.info(
+            "Created new %s for %s",
+            new_pattern.name,
+            stage.name,
+        )
+        return stage
+
+    def _check_milling_stage(self, stage: FibsemMillingStage) -> None:
+        super()._check_milling_stage(stage=stage)
+        if not isinstance(stage.pattern, (BitmapPattern, TrenchBitmapPattern)):
+            raise TypeError(
+                f"Stage pattern type is {type(stage.pattern)}, expected pattern types are BitmapPattern, TrenchBitmapPattern."
+            )
+        elif stage.pattern.array is None:
+            raise AttributeError(
+                f"{stage.pattern.name} attribute 'array' has not been set"
+            )
+        self._check_bitmap(stage.pattern.array)
+
+    def _convert_trench_pattern(
+        self,
+        pattern: TrenchPattern,
+        stats: LamellaStatistics,
+        lamella_gis_boundary_peturbations: NDArray[
+            np.integer[Any] | np.float32 | np.float64
+        ]
+        | None = None,
+        gis_lower_edge_coordinates: NDArray[np.float64 | np.float32 | np.integer[Any]]
+        | None = None,
+    ) -> TrenchBitmapPattern:
+        bitmap_array = self.create_bitmap_array(
+            pattern_width_m=pattern.width,
+            stats=stats,
+            lamella_gis_boundary_peturbations=lamella_gis_boundary_peturbations,
+            gis_lower_edge_coordinates=gis_lower_edge_coordinates,
+        )
+
+        if pattern.time != 0:
+            _logger.warning(
+                "Bitmap adaptive polishing won't work as expected because pattern time has been set"
+            )
+
+        return TrenchBitmapPattern(
+            point=pattern.point,
+            width=pattern.width,
+            spacing=pattern.spacing,
+            depth=pattern.depth,
+            upper_trench_height=pattern.upper_trench_height,
+            lower_trench_height=pattern.lower_trench_height,
+            time=pattern.time,
+            array=bitmap_array,
+            interpolation="bilinear",
+        )
+
+    def _convert_rectangle_pattern(
+        self,
+        pattern: RectanglePattern,
+        stats: LamellaStatistics,
+        lamella_gis_boundary_peturbations: NDArray[
+            np.integer[Any] | np.float32 | np.float64
+        ]
+        | None = None,
+        gis_lower_edge_coordinates: NDArray[np.float64 | np.float32 | np.integer[Any]]
+        | None = None,
+    ) -> BitmapPattern:
+        bitmap_array = self.create_bitmap_array(
+            pattern_width_m=pattern.width,
+            stats=stats,
+            lamella_gis_boundary_peturbations=lamella_gis_boundary_peturbations,
+            gis_lower_edge_coordinates=gis_lower_edge_coordinates,
+        )
+
+        if pattern.time != 0:
+            _logger.warning(
+                "Bitmap adaptive polishing won't work as expected because pattern time has been set"
+            )
+
+        return BitmapPattern(
+            point=pattern.point,
+            width=pattern.width,
+            height=pattern.height,
+            depth=pattern.depth,
+            rotation=pattern.rotation,
+            time=pattern.time,
+            passes=pattern.passes,
+            scan_direction=pattern.scan_direction,
+            array=bitmap_array,
+            interpolation="bilinear",
+        )
+
+    @staticmethod
+    def _get_milling_pixel_dimensions(
+        dimensions: tuple[float, float],
+        fib_pixel_size_m: tuple[float, float],
+    ) -> tuple[int, int]:
+        return (
+            round(dimensions[0] / fib_pixel_size_m[0]),
+            round(dimensions[1] / fib_pixel_size_m[1]),
+        )
+
+    def _create_milling_cycle_plot(
+        self,
+        plots_directory: Path,
+        lamella_info: LamellaInformation,
+        stage: FibsemMillingStage | None,
+    ) -> None:
+        stats = lamella_info.statistics
+        # Create plots
+        create_milling_cycle_plot(
+            save_path=plots_directory / f"{lamella_info.identifier}_plot.png",
+            sem_image=lamella_info.sem_image,
+            fib_image=lamella_info.fib_image,
+            first_prediction=lamella_info.prediction,
+            clean_prediction=lamella_info.clean_prediction,
+            gis_thickness_um=stats.gis_thickness_filtered_um,
+            gis_thickness_min_um=stats.gis_thickness_min_um,
+            gis_thickness_median_um=stats.gis_thickness_median_um,
+            crack_area_um2=stats.crack_area_um2,
+            gis_min_stop_threshold_um=self.config.gis_stop_min * 1e6,
+            gis_median_stop_threshold_um=self.config.gis_stop_median * 1e6,
+            gis_min_threshold_um=self.config.gis_min * 1e6,
+            gis_max_threshold_um=self.config.gis_max * 1e6,
+            milling_stage=stage,
+            image_xlims=stats.xlims_image_px,
+            max_crack_area_um2=self.config.max_crack_area * 1e12,
+            img_name=lamella_info.identifier,
+            pattern_dwell_multiplier=stats.pattern_dwell_multiplier,
+            pattern_xlims=stats.pattern_xlims_px,
+            dwell_multiplier_mean_threshold=self.config.dwell_multiplier_mean_stop,
+            dwell_multiplier_max_threshold=self.config.dwell_multiplier_max_stop,
+        )
+
+    def _refine_xlims(
+        self,
+        lamella_width,
+        gis_thickness: NDArray[np.float64 | np.float32],
+        xlims: tuple[int, int],
+    ) -> tuple[int, int]:
+        if self.config.pattern_alignment == "centre":
+            return crop_xlims_centre(
+                xlims=xlims,
+                lamella_width=lamella_width,
+            )
+        elif self.config.pattern_alignment == "convolve":
+            return crop_xlims_convolve_filtered(
+                xlims=xlims,
+                lamella_width=lamella_width,
+                gis_thickness=gis_thickness,
+            )
+
+        return crop_xlims_centre(lamella_width=lamella_width, xlims=xlims)
+
+    def create_bitmap_array(
+        self,
+        pattern_width_m: float,
+        stats: LamellaStatistics,
+        lamella_gis_boundary_peturbations: NDArray[
+            np.integer[Any] | np.float32 | np.float64
+        ]
+        | None = None,
+        gis_lower_edge_coordinates: NDArray[np.float64 | np.float32 | np.integer[Any]]
+        | None = None,
+    ) -> NDArray:
+        if stats.gis_thickness_filtered_um is None:
+            raise ValueError('"gis_thickness_filtered_um" is not available')
+        elif stats.crack_thickness_prediction_px is None:
+            raise ValueError('"crack_thickness_prediction_px" is not defined')
+        elif stats.xlims_image_px is None:
+            raise ValueError('"xlims_image_px" is not defined')
+
+        lamella_width_px = round(pattern_width_m / stats.image_pixel_size_m[0])
+
+        gis_thickness_filtered_um = np.asarray(stats.gis_thickness_filtered_um)
+
+        pattern_xlims = self._refine_xlims(
+            lamella_width=lamella_width_px,
+            gis_thickness=gis_thickness_filtered_um,
+            xlims=stats.xlims_image_px,
+        )
+        stats.pattern_xlims_px = pattern_xlims
+
+        bitmap_signal = gis_thickness_filtered_um.copy()
+        if (
+            gis_lower_edge_coordinates is not None
+            and self.config.incident_angle_sputter_ratio > 1
+        ):
+            # TODO: is there a neater way to do this?
+            angle_dwell_multiplier = get_angle_dwell_multiplier(
+                gis_lower_edge_coordinates=gis_lower_edge_coordinates,
+                pixel_size=stats.image_pixel_size_m,
+                sputter_ratio=self.config.incident_angle_sputter_ratio,
+            )
+
+            angle_dwell_multiplier = np.interp(
+                np.linspace(
+                    0,
+                    len(stats.lamella_thickness_prediction_px),
+                    len(bitmap_signal),
+                    endpoint=False,
+                ),
+                gis_lower_edge_coordinates[:, 1],
+                angle_dwell_multiplier,
+            )
+
+            if self.config.incident_angle_scale_with_gis_min:
+                # Apply the incident angle scaling to the range between GIS min
+                # and max, rather than the whole range.
+                bitmap_signal[pattern_xlims[0] : pattern_xlims[1] + 1] = (
+                    (
+                        bitmap_signal[pattern_xlims[0] : pattern_xlims[1] + 1]
+                        - self.config.gis_min * 1e6
+                    )
+                    * angle_dwell_multiplier[pattern_xlims[0] : pattern_xlims[1] + 1]
+                ) + self.config.gis_min * 1e6
+            else:
+                bitmap_signal[pattern_xlims[0] : pattern_xlims[1] + 1] *= (
+                    angle_dwell_multiplier[pattern_xlims[0] : pattern_xlims[1] + 1]
+                )
+
+        if lamella_gis_boundary_peturbations is not None:
+            # Interpolate boundary peturbations to have image_px width
+            interpolated_boundary_peturbations_um = np.interp(
+                np.linspace(
+                    0,
+                    len(stats.lamella_thickness_prediction_px),
+                    len(bitmap_signal),
+                    endpoint=False,
+                ),
+                lamella_gis_boundary_peturbations[:, 1],
+                lamella_gis_boundary_peturbations[:, 0]
+                * stats.prediction_pixel_size_m[0]
+                * 1e6,
+            )
+            # We don't want to aim for below gis_min_um, ensure we aren't
+            # increasing any values in bitmap_signal
+            interpolated_boundary_peturbations_um = np.clip(
+                interpolated_boundary_peturbations_um, 0, None
+            )
+            # Remove the dips in to aim for a smoother GIS layer
+            bitmap_signal -= interpolated_boundary_peturbations_um
+
+        if self.config.mask_cracks:
+            # Interpolate cracks to have image_px width
+            crack_thickness_image_px = resize_interp_1d(
+                stats.crack_thickness_prediction_px, target_size=len(bitmap_signal)
+            )
+
+            # Set any region with cracks a thickness of 0 for the purposes of the bitmap
+            bitmap_signal[crack_thickness_image_px > 0] = 0
+
+        filtered_trimmed_bitmap_signal = self._filter_bitmap_signal(
+            bitmap_signal[pattern_xlims[0] : pattern_xlims[1] + 1]
+        )
+
+        bitmap_array = create_bitmap_array(
+            input_signal=filtered_trimmed_bitmap_signal,
+            xlims=(0, len(filtered_trimmed_bitmap_signal) - 1),
+            min_dwell_threshold=self.config.gis_min * 1e6,
+            max_dwell_threshold=self.config.gis_max * 1e6,
+            as_image=False,
+        )
+
+        stats.pattern_dwell_multiplier = bitmap_array[0, :, 0].tolist()
+        stats.pattern_blanking = bitmap_array[0, :, 1].astype(bool).tolist()
+
+        return bitmap_array
+
+    def _filter_bitmap_signal(
+        self,
+        array_1d: NDArray[np.float32 | np.float64],
+    ) -> NDArray[np.float32 | np.float64]:
+        return filter_bitmap_signal(
+            array_1d=array_1d,
+            erosion_px=self.config.bitmap_erosion_px,
+            gaussian_sigma=self.config.bitmap_gaussian_sigma,
+            minimum_value=self.config.gis_min * 1e6,
+            maximum_value=self.config.gis_max * 1e6,
+        )
+
+    def _check_bitmap(self, bitmap_array: NDArray[Any]) -> None:
+        if (
+            dwell_multiplier_max := bitmap_array[:, :, 0].max()
+        ) < self.config.dwell_multiplier_max_stop:
+            raise StopMillingException(
+                f"The maximum dwell time multiplier is {dwell_multiplier_max:.3f}, below the threshold of {self.config.dwell_multiplier_max_stop:.3f}",
+                reason=StopReasons.MAX_DWELL_MULTIPLIER,
+            )
+        elif (
+            dwell_multiplier_mean := bitmap_array[:, :, 0].mean()
+        ) <= self.config.dwell_multiplier_mean_stop:
+            raise StopMillingException(
+                f"The mean dwell time multiplier is {dwell_multiplier_mean:.3f}, below the threshold of {self.config.dwell_multiplier_mean_stop:.3f}",
+                reason=StopReasons.MEAN_DWELL_MULTIPLIER,
+            )
