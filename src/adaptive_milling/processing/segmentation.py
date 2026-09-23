99e0f6d2c219b4c8ddaf233339894041c1785dac
Thomas M Fish <50103643+thomasmfish@users.noreply.github.com>
2026-09-23 13:25:43 +0100

    Skip lamella spot removal if lamella_spot_area is 0 (or below)

--- src/adaptive_milling/processing/segmentation.py
+++ src/adaptive_milling/processing/segmentation.py
@@ -89,10 +89,15 @@ def clean_prediction(
     mask_foreground_lamella = (
         mask_largest_foreground & masks[SEMSegmentationLabels.LAMELLA]
     )
-    lamella_spots = get_spots_in(
-        mask_foreground_lamella,
-        max_spot_area=lamella_spot_area,  # remove lamella spots <= 5 pixels in size
-    )
+
+    if lamella_spot_area > 0:
+        lamella_spots = get_spots_in(
+            mask_foreground_lamella,
+            max_spot_area=lamella_spot_area,  # remove lamella spots <= 5 pixels in size
+        )
+    else:
+        lamella_spots = np.zeros_like(mask_foreground_lamella)
+
     mask_foreground_lamella = mask_foreground_lamella & ~lamella_spots
 
     mask_foreground_gis = mask_largest_foreground & masks[SEMSegmentationLabels.GIS]
